d53f829ab9ddc4726a0dfca28d45eb8a58c05101
sirdedz <darbyedwards1@gmail.com>
2021-06-30 15:10:55 +0800

    Mobile friendly menu added

--- app/static/main.js
+++ app/static/main.js
@@ -5,6 +5,29 @@ function toHome(){
     window.location.replace("/");
 }
 
+function menu(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        $(".header-left").children().each(function(){
+            if($(this).is("p")){
+                if($(this).css('display') == 'none'){
+                    $(this).css("display", "grid");
+                }else{
+                    $(this).css("display", "none");
+                }
+            }
+        })
+        if($(".header-right").css('display') == 'none'){
+            $(".header-right").css("display", "flex");
+            $(".menu i").removeClass("bi-caret-down-fill");
+            $(".menu i").addClass("bi-caret-up-fill");
+        }else{
+            $(".header-right").css("display", "none");
+            $(".menu i").addClass("bi-caret-down-fill");
+            $(".menu i").removeClass("bi-caret-up-fill");
+        }
+    }
+}
+
 //INDEX.HTML
 $('.carousel').carousel({
     interval: 10000
