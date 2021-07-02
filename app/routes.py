8eba81bb5734419e0e5e835de26860aecb7f4895
sirdedz <darbyedwards1@gmail.com>
2021-07-02 17:16:53 +0800

    Alpha version ready for production without content. (Lorem Ipsum)

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,17 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', home=1)
+
+
+@app.route('/citi')
+def citi():
+    return render_template('citi.html', home=0)
+
+@app.route('/sharkstakes')
+def sharkstakes():
+    return render_template('sharkstakes.html', home=0)
+
+@app.route('/python_game')
+def python_game():
+    return render_template('python_game.html', home=0)
--- app/static/main.js
+++ app/static/main.js
@@ -33,12 +33,97 @@ function menu(){
     }
 }
 
+function carouselShower(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        console.log('as');
+        $("#carousel").hide();
+    }else{
+        console.log("bs");
+    }
+}
+
 function imageSize(){
     var ch = $('#profilepic').height();
     $('#profilepic').css({'width':ch+'px'});
 }
 
+function find(name){
+    menu();
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("." + name).offset().top - 50
+    }, 1500);
+}
+
+function toTop(){
+	jQuery('html, body').animate({scrollTop:0},1000);
+}
+
+$(document).scroll(function(){
+    if($(window).scrollTop() >= $(window).height() && $("#totop").css('display') == 'none'){
+        $("#totop").show("slow");
+    }
+
+    if($(window).scrollTop() < $(window).height() && $("#totop").css('display') != 'none'){
+        $("#totop").hide("slow");
+    }
+
+    $(".py-game").children().each(function(){
+        if($(this).offset().top <= $(window).scrollTop() + $(window).height() && !$(this).hasClass(".py-title") && !$(this).hasClass(".py-right-animation") && !$(this).hasClass(".py-right-animation")){
+            $(this).children('.py-text-right').addClass('py-right-animation');
+            $(this).children('.py-text-left').addClass('py-left-animation');
+        }
+    });
+});
+
 //INDEX.HTML
 $('.carousel').carousel({
     interval: 10000
 });
+
+$(document).ready(function(){
+
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+
+        $("#pdfviewer-web").show();
+
+        $('.mobile-slider').show();
+        $('.projects').hide();
+
+        $('.mobile-slider').slick({
+            centreMode: true,
+            centrePadding: "50px",
+            slidesToShow: 1,
+            dots: false,
+            arrows: false,
+            adaptiveHeight: false,
+            cssEase: "ease",
+        });
+
+        $('.slick-slide').eq(1).addClass("slide-animation-forwards");
+
+        $('.mobile-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            if($('.slick-slide').eq(nextSlide+1 % 3).hasClass("slide-animation-reverse")){
+                $('.slick-slide').eq(nextSlide+1 % 3).removeClass("slide-animation-reverse");
+            }
+            
+            if(!$('.slick-slide').eq(nextSlide+1 % 3).hasClass("slide-animation-forwards")){
+                $('.slick-slide').eq(nextSlide+1 % 3).addClass("slide-animation-forwards");
+            }
+
+            if($('.slick-slide').eq(currentSlide+1 % 3).hasClass("slide-animation-forwards")){
+                $('.slick-slide').eq(currentSlide+1 % 3).removeClass("slide-animation-forwards");
+            }
+
+            if(!$('.slick-slide').eq(currentSlide+1 % 3).hasClass("slide-animation-reverse")){
+                $('.slick-slide').eq(currentSlide+1 % 3).addClass("slide-animation-reverse");
+            }
+        });
+    }else{
+        $('.mobile-slider').hide();
+        $('#pdfviewer-mobile').hide();
+    }
+});
+          
+
+//CITI.HTML
